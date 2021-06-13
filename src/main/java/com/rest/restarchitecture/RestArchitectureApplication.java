57a2042f6c943022853ac4ae3ad0c203b45c732e
Anatolii Sichkar <anatolii_sichkar@epam.com>
2021-06-13 15:39:30 +0300

    first commit

---
+++ src/main/java/com/rest/restarchitecture/RestArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.rest.restarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/restarchitecture/RestArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.restarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
